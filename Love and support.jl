cf87b73c145d6e18f47f76e7e4e38590ac141a7e
ymukhina <yulia.mukhina@lix.polytechnique.fr>
2024-05-29 13:53:42 +0200

    Add files via upload

--- Love and support.jl
+++ Love and support.jl
@@ -1,4 +1,5 @@
 using Oscar
+using Nemo
 using StructuralIdentifiability
 using IterTools
 import StructuralIdentifiability: _reduce_mod_p, reduce_ode_mod_p, power_series_solution, ps_diff 
@@ -21,20 +22,20 @@ function f_min_support_bivariate(ode::ODE)
         A = [1 d1 (2 * d1 - 1); 1 d2 (d1 + d2 - 1); -1 0 0; 0 -1 0; 0 0 -1]
         b = [d1*(2*d1 - 1), d1*(d1 + d2 - 1), 0, 0, 0]
     end
-    return lattice_points(polyhedron(A, b))
+    return collect(lattice_points(polyhedron(A, b)))
 end
 
-function f_min_support(ode::ODE)
+function f_min_support(ode::ODE; info = false)
     # I guess by sorting you try to ensure that the first x is the output, i am not sure
     # this would always work. You could just choose x such that the only y_equation is equal to x
     x = sort(ode.x_vars, rev = true)
-    @info "Inferred order $x"
+    info && @info "Inferred order $x"
     n = length(x)
     gs = [ode.x_equations[xi] for xi in x]
     d1 = total_degree(gs[1])
     @assert d1 > 0 "d1 = 0"
     D = maximum(total_degree, gs[2:end])
-    @info "We have d1 = $d1 and D = $D"
+    info && @info "We have d1 = $d1 and D = $D"
 
     if d1 <= D
         ineq_lhs = reshape([1, [d1 + (k - 1) * (D - 1) for k in 1:n]...], 1, n + 1)
@@ -54,7 +55,7 @@ function f_min_support(ode::ODE)
         A = vcat(matrix(QQ, ineq_lhs1 + ineq_lhs2), -identity_matrix(QQ, n + 1))
         b = vcat(ineq_rhs, zeros(QQ, n + 1))
     end
-    return lattice_points(polyhedron(A, b))
+    return collect(lattice_points(polyhedron(A, b)))
 end
 
 # ---------------------------------------------------------------------- #
@@ -62,7 +63,7 @@ end
 
 # -------- Compute f_min using an ansatz equation -------- #
 
-function solve_with_love_and_support(ode::ODE, p::Int)
+function solve_with_love_and_support(ode::ODE, p::Int; info = true)
     @assert is_probable_prime(p) "This is not a prime number, Yulia!"
 
     ode_mod_p = reduce_ode_mod_p(ode, p)
@@ -73,7 +74,8 @@ function solve_with_love_and_support(ode::ODE, p::Int)
 
     # compute Newton polytope of f_min
     possible_supp = f_min_support(ode)
-    @info "The size of the estimates support is $(length(possible_supp))"
+                l = length(possible_supp)
+    info && @info "The size of the estimates support is $(length(possible_supp))"
     nterms = length(possible_supp) + n
 
     # random initial conditions
@@ -83,33 +85,39 @@ function solve_with_love_and_support(ode::ODE, p::Int)
     inp = empty(Dict(x[1] => [1]))
 
     ps_soltime = @elapsed ps_sol = power_series_solution(ode_mod_p, par, ic, inp, nterms)
-    @info "Power series solution computed in $ps_soltime"
+    info && @info "Power series solution computed in $ps_soltime"
 
     start_system_time = time()
     pss = [ps_sol[y]]
     for i in 1:n
         push!(pss, ps_diff(pss[end]))
     end
+        
+    sort!(possible_supp, by = sum)
+    prods = eval_at_support(possible_supp, pss)
+                    
+    #prods = [prod([pss[k]^exp[k] for k in 1:length(pss)]) for exp in possible_supp]                
 
     # Gleb: how about broadcast: pss .^ exp ?
-    prods = [prod([pss[k]^exp[k] for k in 1:length(pss)]) for exp in possible_supp]
+                
+                    
 
     ls = matrix([coeff(pr, j) for j in 0:(nterms - n - 1), pr in prods])
-    @info "System created in $(time() - start_system_time)"
+    info && @info "System created in $(time() - start_system_time)"
 
-    @info "linear system dims $(size(ls))"
+    info && @info "linear system dims $(size(ls))"
     
-    system_soltime = @elapsed ker = kernel(ls, side=:right)
-    @info "Linear system solved in $system_soltime"
-    
-    @info "The dimension of the solution space is $(size(ker))"
-    sol = ker[:, 1]
-
+    system_soltime = @elapsed ker = kernel(ls, side=:right)[2]
+    info && @info "Linear system solved in $system_soltime"
+            dim = kernel(ls, side=:right)[1]
+    info && @info "The dimension of the solution space is $(dim)"
+                    
     R, _ = polynomial_ring(F, ["x1", ["x1^($i)" for i in 1:n]...])
-
     mons = [prod([gens(R)[k]^exp[k] for k in 1:ngens(R)]) for exp in possible_supp]
 
-    return sum([s * m for (s, m) in zip(sol, mons)])
+    g = gcd([sum([s * m for (s, m) in zip(ker[:, i], mons)]) for i in 1:dim])                  
+  
+    return g
 end
 
 # -------------------------------------------------------- #
@@ -142,6 +150,22 @@ function check_ansatz(ode::ODE, p::Int)
     quot, rem = divrem(phi(io_tocheck), io_correct)
     return iszero(rem) && iszero(total_degree(quot))
 end
+                    
+                    
+                    
+# Counting the integer points inside guessed polytopes and the ones from the theorem
+
+function bound_difference(d1::Int, d2::Int, p::Int) # counting for systems [d1, d2, 1:p]
+            for i in 1:p
+                @info "System | Num of the int points inside the polytope | Num of monomials in the min pol"
+                ode = rand_ode([d1,d2,i])
+                s = size(f_min_support(ode))[1]
+                l = length(solve_with_love_and_support(ode, Int(rand_bits_prime(ZZ, 32)), info = true))            
+                @info "[$d1,$d2,$i] | $s | $l"
+            end      
+end
+
+
 
 # ------------------------------- #
 
@@ -175,10 +199,33 @@ function rand_ode(degs::Vector{Int})
         Ptype[]
     )
 end
+                    
+#little friendly helper
+function one_thing(e, vals, cacher)
+    if !haskey(cacher, e)
+        for i in 1:length(e)
+            if e[i] != 0 #[1,?]
+                    e_love = copy(e)
+                    e_love[i] -= 1
+                    res = vals[i] * one_thing(e_love, vals, cacher)
+                    cacher[e] = res
+            end
+         end  
+    end
+    return cacher[e]
+end    
+
+# Want dictionary from supp to the evaluate value
+function eval_at_support(supp, vals)
+    s = sort(supp, by = sum) 
+    cacher = Dict(s[1] => one(parent(first(vals))))
+        for e in s[2:end] #index
+            one_thing(e, vals, cacher)                 
+        end
+    return return [cacher[s] for s in supp]
+end                    
+                    
 
 # ---------------------------------- #
-
-
-
 
 
