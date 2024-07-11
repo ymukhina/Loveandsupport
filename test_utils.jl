69b25f881566645a1f4c8da24be4f0da93ce5dca
Yulia Mukhina <yulia.mukhina@Yulias-MacBook-Pro.local>
2024-07-11 12:23:07 +0200

    working check_anzatz

--- test_utils.jl
+++ test_utils.jl
@@ -7,34 +7,6 @@ import StructuralIdentifiability: _reduce_mod_p, reduce_ode_mod_p, power_series_
 const Ptype = QQMPolyRingElem
 
 # -------- Test function -------- #
-# test interpolation_ansatz against IO enemy equation 
-function check_interpol_ansatz_modp(ode::ODE, p::Int)
-
-    @info "Solving with love and support!"
-    tim = @elapsed io_tocheck = interpolation_with_love_and_support_modp(ode, p)
-    @info "time: $(tim) seconds"
-
-    @info "Solving without love and support :("
-    tim = @elapsed io_correct = first(values(find_ioequations(ode)))
-    io_correct = _reduce_mod_p(io_correct, p)
-    @info "time: $(tim) seconds"
-
-    R = parent(io_tocheck)
-    S = parent(io_correct)
-
-    # the variables of io_correct
-    n = ngens(R)
-    ys = gens(S)[2 * (n - 1) + 2 : 3 * (n - 1) + 2]
-    @info "IO variables $(ys)"
-
-    phi = hom(R, S, ys)
-
-    quot, rem = divrem(phi(io_tocheck), io_correct)
-    return iszero(rem) && iszero(total_degree(quot))
-end
-    
-
-
 
 # test ansatz against IO enemy equation 
 function check_ansatz_modp(ode::ODE, p::Int)
@@ -73,28 +45,33 @@ end
 function check_ansatz(ode::ODE)
 
     @info "Solving with love and support!"
-    tim = @elapsed io_tocheck = eliminate_with_love_and_support(ode)
+    tim = @elapsed io_tocheck = eliminate_with_love_and_support(ode, rand(1:2^30))[1]
+            println(io_tocheck)
     @info "time: $(tim) seconds"
-    println(Oscar.terms(io_tocheck))
+   
 
     @info "Solving without love and support :("
     tim = @elapsed io_correct = first(values(find_ioequations(ode)))
-    io_correct *= (Nemo.leading_coefficient(io_correct)^(-1))
+    io_correct *= (Oscar.leading_coefficient(io_correct)^(-1))
+        println(io_correct)
     @info "time: $(tim) seconds"
-    println(Oscar.terms(io_correct))
+   
 
     R = parent(io_tocheck)
     S = parent(io_correct)
-
-    # the variables of io_correct
+        
     n = ngens(R) 
-    ys = gens(S)[2 * (n - 1) + 2 : 3 * (n - 1) + 2]
-    @info "IO variables $(ys)"
+  
+    svnames = (string).(S.S)
+    y_index = findfirst(vname -> vname == "y(t)_0", svnames)
+    ys = gens(S)[y_index:y_index+(n-1)]
+    @info "IO variables $(ys)"    
 
     phi = hom(R, S, ys)
     quot, rem = divrem(phi(io_tocheck), io_correct)
     # all(c -> c in Oscar.coefficients(io_tocheck), Oscar.coefficients(io_correct))
-    Oscar.coefficients(io_tocheck) .- Oscar.coefficients(io_correct)
+    #Oscar.coefficients(io_tocheck) .- Oscar.coefficients(io_correct)
+    return iszero(rem) && iszero(total_degree(quot))
 end
                 
                     
@@ -106,7 +83,7 @@ function bound_difference(d1::Int, d2::Int, p::Int) # counting for systems [d1, 
     for i in 1:p
         @info "System | Num of the int points inside the polytope | Num of monomials in the min pol"
         ode = rand_ode([d1,d2,i])
-        s = size(f_min_support(ode))[1]
+        s = size(f_min_support(ode, jacobian_check(ode)))[1]
         l = length(eliminate_with_love_and_support_modp(ode, Int(rand_bits_prime(ZZ, 32)), info = true))            
         @info "[$d1,$d2,$i] | $s | $l"
      end      
