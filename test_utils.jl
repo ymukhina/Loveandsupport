05cc124412d11509997d032e9cd91b136ca03dd7
Yulia Mukhina <yulia.mukhina@Yulias-MacBook-Pro.local>
2024-09-10 15:45:02 +0200

    experiments

--- test_utils.jl
+++ test_utils.jl
@@ -92,7 +92,7 @@ Code for the Section 4.1 of the article.
 function test_table(p::Int)
             
    generics = [
-    [3, 1, 1],               
+    [3, 2, 2],               
 ] 
             
       for i in 1:p 
