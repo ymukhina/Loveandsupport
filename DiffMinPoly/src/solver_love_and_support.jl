49faf8b9bdf8d7e0706d393f9bb0c42c23f87f58
MaximeDespreaux <maxime.despreaux@polytechnique.edu>
2025-02-12 17:22:16 +0100

    Fixed initialization of A,B,C and kernel_block implemented kernel_block function

--- DiffMinPoly/src/solver_love_and_support.jl
+++ DiffMinPoly/src/solver_love_and_support.jl
@@ -7,68 +7,42 @@ using Random
 
 const Ptype = QQMPolyRingElem
 
-
-#Finding the kernel of [[A, 0],[B, C]]
-function kernel_block(F, ls_UL, ls_LL, ls_LR) 
+function kernel_block(ls_UL, ls_LL, ls_LR) 
     strt = time()
 
+    F = base_ring(ls_UL)
     v1 = kernel(ls_UL, side=:right)
-    
-    #if A not full rank            # This is the chosen representation: [[A, 0], [B, C]]  a lower triangular in 4 blocks
-    if rank(ls_UL) < size(ls_UL, 2)     # Start of a logic to handle linear combinations for sparse systems
-                                        # Issue is that setting x1 = 0 makes many 0 appear in the matrix A
-                               # Kernel(A) is then [[0,...,0],[1,0,...,0], ..., [0,...,0,1]] effectively describing all
-                                      # possible vectors, hence the infinite solution triggering the error
-                                                                            # "Unable to solve linear system"
-        n_temp, m_temp = size(v1)
-        ls_n, ls_m = size(ls_LR)
-        S = matrix_space(F, ls_n + n_temp, ls_m + m_temp)
-        aug = zero(S)      
-        for i in 1:ls_n
-            for j in 1:ls_m
-                aug[i, j] = ls_LR[i, j]
-            end
-        end
 
-        for i in 1:m_temp
-            w_i = ls_LL * v1[:, i]
-            for j in 1:n_temp
-                aug[ls_n + j, ls_m + i] = w_i[j]
-            end
-        end     #Creating the augmented matrix [C|W] such that ker([C|W]) = [v2|lambda_coeffs]^T
-                # Theoretically should satisfy Cv2 + Bv1' = 0 such that v1' = sum(lambda_i*v1_column(i))
-                # Seems to be that for A not full rank, kernel of A is spanned by canonical vectors and [0,..,0]
-                # This makes the set of solutions infinite, hence unsolvable
-                # We can see that the v2 determined by this has linearly dependent rows
-        v = kernel(aug, side=:right)
-        v2, lambs = v[1:ls_m,:], v[ls_m+1:ls_m + m_temp, :]
-
-        ker = map(1:size(lambs, 2)) do i
-            v1_lamb = zero(v1[:, 1]) 
+    n_temp, m_temp = size(v1)
+    ls_n, ls_m = size(ls_LR)
+    S = matrix_space(F, ls_n, ls_m + m_temp)
+    aug = zero(S)     
         
-            for j in 1:size(v1, 2)
-                v1_temp = zero(v1[:, j])
-                for k in 1:size(v1, 1)    # Scale each element manually since     for j in 1:size(v1, 2)
-                                                                                        #v1_lamb += lambs[j, i] * v1[:, j]
-                                                                                # end
-                                            # MethodError: no method matching *(::fpFieldElem, ::Vector{fpFieldElem})
-                                            #Yet another naive workaround 
-                    v1_temp[k] = lambs[j, i] * v1[k, j]
-                end
-                v1_lamb += v1_temp  
-            end
-        
-            v2_i = v2[:, i]
-            vcat(v1_lamb, v2_i)  # Recover the correct v1 in Ker(A) as a linear combination of basis vectors of ker(A)
+    for i in 1:ls_n
+        for j in 1:ls_m
+            aug[i, j] = ls_LR[i, j]
         end
-
-
-    else
-        temp_LL = -(ls_LL * v1)      # Logic that used to work for dense systems
-        v2 = solve(ls_LR, temp_LL, side=:right)
-        ker = vcat(v1, v2)
-
     end
+
+    for j in 1:m_temp
+        w_i = ls_LL * v1[:, j]
+        aug[:, ls_m + j] = w_i
+    end
+
+
+    v = kernel(aug, side=:right)
+    v2, lambs = v[1:ls_m,:], v[ls_m+1:ls_m + m_temp, :]
+
+    S = matrix_space(F, size(v1, 1), 1)
+    v1_lamb = zero(S)
+    ker = Matrix{eltype(v1)}(undef, size(v1, 1) + size(v2, 1), size(lambs, 2))
+
+    for i in 1:size(lambs, 2)
+        v1_lamb = sum(lambs[j, i] .* v1[:, j] for j in 1:size(v1, 2))
+        ker[:, i] = vcat(v1_lamb, v2[:, i])
+    end
+
+    ker = matrix_space(F, size(ker,1), size(ker,2))(ker)
 
     @info "Linear System solved in $(time() - strt)"
 
@@ -90,8 +64,6 @@ function eliminate(ode::ODE, x, prob = 0.99)
                                                                            
     @assert x in ode.x_vars
     minimal_poly, starting_prime = eliminate_with_love_and_support(ode, x, rand(2^25:2^32 - 1))
-
-    # add a comment
                                                                                                                 
     check = min_pol -> begin
         if isone(prob)
@@ -102,8 +74,7 @@ function eliminate(ode::ODE, x, prob = 0.99)
     end
     
     while check(minimal_poly) == false
-        @info "Running Love & Support again. Incorrect Minimal Polynomial :("
-        # error("STOP FOR TESTING")
+        @info "Incorrect minimal polynomial. Running Love & Support again (T_T)"
         starting_prime = Hecke.next_prime(starting_prime)
         minimal_poly, starting_prime = eliminate_with_love_and_support(ode, x, starting_prime)
     end
@@ -133,50 +104,35 @@ function eliminate_with_love_and_support_modp(ode::ODE, x, p::Int, ord::Int=minp
     l = length(possible_supp)
     info && @info "The size of the estimates support is $(length(possible_supp))"
 
-    if n < 1  #Very temporary fix, but it seems to hold that Optimization1 removes too much info in non dense system of 2 variables.
-        #Furthermore, the runtime improvement gained from the reduction isn't worth the loss in space and information for such systems
-        #Especially considering that for small supports, e.g. here the planar case, the Original Love & Support is already very good.
-        tim2 = @elapsed M = build_matrix_multipoint(F, ode_mod_p, x_mod_p, ord, possible_supp, info = info)
+    tim2 = @elapsed ls_UL, ls_LL, ls_LR = build_matrix_multipoint(F, ode_mod_p, x_mod_p, ord, possible_supp, info = info)
+                                                                    
+    info && @info "Building Linear System in: $(tim2)"
 
-        info && @info "Building Linear System in: $(tim2)"
+    info && @info "Linear Systems with dims $(size(ls_UL)), $(size(ls_LL)) and $(size(ls_LR))"
 
-        strt = time()
+    # strt = time()
 
-        ker = kernel(M, side=:right)
+    # v1 = kernel(ls_UL, side=:right)
+    # temp_LL = -(ls_LL * v1)
+    # v2 = solve(ls_LR, temp_LL, side=:right)
+    # ker = vcat(v1, v2)
 
-        info && @info "Linear System solved in $(time() - strt)"
+    # info && @info "Linear System solved in $(time() - strt)"
 
-    else
-        # This is a modified version of build_matrix_multipoint that should create only A, B and C 
-        # without slicing and initializing M = [[A, 0], [B, C]]
-        tim2 = @elapsed ls_UL, ls_LL, ls_LR = build_matrix_multipoint_big(F, ode_mod_p, x_mod_p, ord, possible_supp, info = info)
-
-        info && @info "Building Linear System in: $(tim2)"
-
-        info && @info "Linear Systems with dims $(size(ls_UL)), $(size(ls_LL)) and $(size(ls_LR))"
-
-        ker = kernel_block(F, ls_UL, ls_LL, ls_LR)
-
-    end
-
-
-    if ndims(ker) == 1  # Check if ker is a 1D vector (n,)
-        ker = reshape(ker, size(ker, 1), 1)  # Convert to (n,1)
-    end
-
-    dim = size(ker, 2)    
-
+    ker = kernel_block(ls_UL, ls_LL, ls_LR)
+    
+    dim = size(ker)[2]
     info && @info "The dimension of the solution space is $(dim)"
 
-    start_constructing_time = time()
+    strt = time()
 
     R, _ = polynomial_ring(F, [var_to_str(x), [var_to_str(x) * "^($i)" for i in 1:ord]...])
             
     mons = [prod([gens(R)[k]^exp[k] for k in 1:ngens(R)]) for exp in possible_supp]
     
-    g = gcd([sum([s * m for (s, m) in zip(ker[:, i], mons)]) for i in 1:dim])  #This breaks for similar reason as * in kernel_block ((((((
-                                                                            # I'll get to it tomorrow
-    info && @info "The resulting polynomial computes in $(time() - start_constructing_time)"
+    g = gcd([sum([s * m for (s, m) in zip(ker[:, i], mons)]) for i in 1:dim])
+       
+    info && @info "The resulting polynomial computes in $(time() - strt)"
 
     return g * (1 // Oscar.leading_coefficient(g))
 end
@@ -359,8 +315,8 @@ function f_min_support(ode::ODE, x, jacobian_rank::Int; info = true)
         A = vcat(matrix(QQ, ineq_lhs1 + ineq_lhs2), -identity_matrix(QQ, n + 1))
         b = vcat(ineq_rhs, zeros(QQ, n + 1))
     end
-    return sort_gleb_max!(collect(lattice_points(Oscar.polyhedron(A, b))))  # sort_gleb but with first the k1 [0, a, b] elements of the support
-end                                                                         # then normal sort_gleb
+    return sort_gleb_max!(collect(lattice_points(Oscar.polyhedron(A, b))))
+end
 
 # -------- Functions for test of correctness -------- #
                     
@@ -402,69 +358,15 @@ end
 function build_matrix_multipoint(F, ode, x, minpoly_ord, support; info = true)
     var_to_sup = var_ind -> [(k == var_ind) ? 1 : 0 for k in 1: (minpoly_ord + 1) ]                                           
     n = length(ode.x_vars)
-    @info "computing derivatives"
-    dervs = lie_derivatives(minpoly_ord, ode, x)
-    @info "done"
-
-    support = [Vector{Int64}(p) for p in support]
-    
-    lsup = length(support)                                                    
-    S = matrix_space(F, lsup, lsup)
-    M = zero(S)
-    supp_to_index = Dict(s => i for (i, s) in enumerate(support))
-
-    # filling the columns corresponding to the derivatives
-    for i in 1:lsup
-        M[i, 1] = 1
-        vec = [rand(F) for _ in 1:(n + 1) ]                                      
-        evals = [derv(vec...) for derv in dervs]
-        
-        
-        for j in 1:(minpoly_ord + 1)
-            supp = var_to_sup(j)
-            ind = supp_to_index[supp]
-            M[i, ind] = evals[j]
-        end
-    end
-
-    # filling the rest of the columns
-    for i in (minpoly_ord + 3):lsup
-        supp = support[i]
-        supp_divisor = copy(supp)
-        nonzero_ind = findfirst(x -> x > 0, supp_divisor)
-        supp_divisor[nonzero_ind] -= 1                                                 
-        multiplier = zeros(Int, minpoly_ord + 1)
-        multiplier[nonzero_ind] += 1
-        while !haskey(supp_to_index, supp_divisor)
-            nonzero_ind = findfirst(x -> x > 0, supp_divisor)
-            supp_divisor[nonzero_ind] -= 1
-            multiplier[nonzero_ind] += 1
-        end                                                    
-        
-        supp_div_ind = supp_to_index[supp_divisor]
-        mult_ind = get(supp_to_index, multiplier, -1)
-        for j in 1:lsup
-            if mult_ind == -1
-                multiplier_eval = prod(M[j, 2:(minpoly_ord + 2)] .^ multiplier)
-            else
-                multiplier_eval = M[j, mult_ind]
-            end
-            M[j, i] = M[j, supp_div_ind] * multiplier_eval           
-        end
-    end
-      
-  return M
-end
-
-# This function assumes that support contains the unit vectors and is sorted by `sort_gleb_max!`
-function build_matrix_multipoint_big(F, ode, x, minpoly_ord, support; info = true)
-    var_to_sup = var_ind -> [(k == var_ind) ? 1 : 0 for k in 1: (minpoly_ord + 1) ]                                           
-    n = length(ode.x_vars)
 
     tim2 = @elapsed dervs = lie_derivatives(minpoly_ord, ode, x)
     info && @info "Computing Derivatives in: $(tim2)"
 
+
+    # println(dervs)
     support = [Vector{Int64}(p) for p in support]
+
+    # println(support)
     lsup = length(support)  
     k1 = 0                     
     for i in 1:lsup
@@ -484,89 +386,137 @@ function build_matrix_multipoint_big(F, ode, x, minpoly_ord, support; info = tru
     C = zero(S)                                     # depend on x1, e.g. each term of x1' has x1 and each term of x1'' has x1 for example
     supp_to_index1 = Dict(s => i for (i, s) in enumerate(s1))  #first part of supp with x1^0
     supp_to_index2 = Dict(s => i for (i, s) in enumerate(s2))  #second part of supp with x1^k with k != 0
+    # vecs = [[F(1), F(1), rand(F)], [F(1), F(2), rand(F)], [F(2), F(1), rand(F)], [F(2), F(2), rand(F)], [F(3), F(1), rand(F)],[F(2), F(3), rand(F)],[F(3), F(3), rand(F)]]
+    memo_eval = Dict()
 
 
     # filling the columns corresponding to the derivatives   
     for i in 1:k1                 
         A[i, 1] = 1    
-        vec = [rand(F) for _ in 1:(n + 1)]               #Similar to previous implementation
+        vec = [rand(F) for _ in 1:(n + 1) ] 
         vec[1] = F(0)                                   # with x1 = 0 
 
-        evals = [derv(vec...) for derv in dervs]      
+        evals = [derv(vec...) for derv in dervs]   
 
-        for j in 2:(minpoly_ord + 1)            # since A shoudln't have terms containing x1,
-            supp = var_to_sup(j)                # no need to look for the [1,0,0] in s1 
+        for j in 2:(minpoly_ord + 1)
+            supp = var_to_sup(j)
+            memo_eval[supp] = evals[j]
             ind = supp_to_index1[supp]   
             A[i, ind] = evals[j]          #Should fill in the [1, x1', x1'', combinations of x1' and x1'']
         end
+        
+        for j in 2:k1           
+            supp = s1[j]
+            if haskey(memo_eval, supp)   #check if we already evaluated and assigned in A
+                # println("Already have $supp with value $(memo_eval[supp])")   #if yes, then skip since it's the evals case
+                continue
+            end
+
+            ind = supp_to_index1[supp]
+
+            res = 0
+            sup_elem = copy(supp)
+            multiplier = zeros(Int, minpoly_ord + 1)
+            nonzero_ind = findfirst(x -> x > 0, sup_elem)
+            sup_elem[nonzero_ind] -= 1                                                
+            multiplier[nonzero_ind] += 1
+            while !(haskey(memo_eval, sup_elem))
+                ord_ind = findfirst(x -> x > 0, sup_elem)
+                sup_elem[nonzero_ind] -= 1                                                
+                multiplier[nonzero_ind] += 1    
+            end
+
+            res += memo_eval[multiplier] * memo_eval[sup_elem]
+            memo_eval[supp] = res
+
+            A[i, ind] = res
+        end
+        empty!(memo_eval)
     end
+
 
     for i in 1:(lsup - k1)                 
         B[i, 1] = 1    
-        vec = [rand(F) for _ in 1:(n + 1)]         #Similar to A, but without setting x1 = 0
+        vec = [rand(F) for _ in 1:(n + 1) ] 
 
-        evals = [derv(vec...) for derv in dervs]      
+        evals = [derv(vec...) for derv in dervs]   
 
-        for j in 2:(minpoly_ord + 1)          #Same logic as for A
-            supp = var_to_sup(j)     
-            ind = supp_to_index1[supp]   
-            B[i, ind] = evals[j]        
-        end
-                  
-        for j in 1:(lsup - k1)      #Using the same vec as for B to build C
-            sup = s2[j]
-            val = 1
-            for s in 1:length(sup)
-                val *= evals[s]^(sup[s])  #<-- should make this better eventually, very naive implementation for now
+        for j in 1:(minpoly_ord + 1)
+            supp = var_to_sup(j)
+            memo_eval[supp] = evals[j]
+            if j > 1
+                ind = supp_to_index1[supp]   
+                B[i, ind] = evals[j]          #Should fill only in the [1, x1', x1'', combinations of x1' and x1'']
             end
-            C[i, j] = val                   #Filling all of C with the same interpolation vectors as for B for consistency
-        end       
-    end
-
-
-    # filling the rest of the columns
-    for i in (minpoly_ord + 3):k1
-        supp = support[i]
-        supp_divisor = copy(supp)
-        nonzero_ind = findfirst(x -> x > 0, supp_divisor)   #Rest of the logic is the same as before
-        supp_divisor[nonzero_ind] -= 1                                                
-        multiplier = zeros(Int, minpoly_ord + 1)
-        multiplier[nonzero_ind] += 1
-        while !(haskey(supp_to_index1, supp_divisor) || haskey(supp_to_index2, supp_divisor))
-            nonzero_ind = findfirst(x -> x > 0, supp_divisor)
-            supp_divisor[nonzero_ind] -= 1
-            multiplier[nonzero_ind] += 1
-        end                                                    
-
-        if supp_divisor[1] == 0
-            supp_div_ind = supp_to_index1[supp_divisor]
-            mult_ind = get(supp_to_index1, multiplier, -1)
-        else
-            supp_div_ind = supp_to_index2[supp_divisor]
-            mult_ind = get(supp_to_index2, multiplier, -1)
         end
-
         
-        for j in 1:k1      #Simply treat the A and B cases. The C case was naively implemented above temporarely
-            if mult_ind == -1
-                multiplier_eval = prod(A[j, 2:(minpoly_ord + 2)] .^ multiplier)
-            else
-                multiplier_eval = A[j, mult_ind]
+        for j in 2:k1           
+            supp = s1[j]
+
+            if haskey(memo_eval, supp)
+                continue
             end
-            A[j, i] = A[j, supp_div_ind] * multiplier_eval          
+
+            ind = supp_to_index1[supp]
+
+            res = 0
+            sup_elem = copy(supp)
+            multiplier = zeros(Int, minpoly_ord + 1)
+            nonzero_ind = findfirst(x -> x > 0, sup_elem)
+            sup_elem[nonzero_ind] -= 1                                                
+            multiplier[nonzero_ind] += 1
+            while !(haskey(memo_eval, sup_elem))
+                ord_ind = findfirst(x -> x > 0, sup_elem)
+                sup_elem[nonzero_ind] -= 1                                                
+                multiplier[nonzero_ind] += 1    
+            end
+
+            res += memo_eval[multiplier] * memo_eval[sup_elem]
+            memo_eval[supp] = res
+
+            B[i, ind] = res
         end
 
         for j in 1:(lsup - k1)
-            if mult_ind == -1
-                multiplier_eval = prod(B[j, 2:(minpoly_ord + 2)] .^ multiplier)
-            else
-                multiplier_eval = B[j, mult_ind]
+            if j == 1
+                C[i, 1] = evals[1]
+                continue
             end
-            B[j, i] = B[j, supp_div_ind] * multiplier_eval          
+
+            supp = s2[j]
+            
+            if haskey(memo_eval, supp)
+                continue
+            end
+
+            ind = supp_to_index2[supp]
+
+            res = 0
+            sup_elem = copy(supp)
+            multiplier = zeros(Int, minpoly_ord + 1)
+            nonzero_ind = findfirst(x -> x > 0, sup_elem)
+            sup_elem[nonzero_ind] -= 1                                                
+            multiplier[nonzero_ind] += 1
+            while !(haskey(memo_eval, sup_elem))
+                ord_ind = findfirst(x -> x > 0, sup_elem)
+                sup_elem[nonzero_ind] -= 1                                                
+                multiplier[nonzero_ind] += 1    
+            end
+
+            res += memo_eval[multiplier] * memo_eval[sup_elem]
+            memo_eval[supp] = res
+
+            C[i, ind] = res
         end
+
+        empty!(memo_eval)
     end
 
-  return A, B, C
+    # println(A)
+    # println(B)
+    # println(C)
+
+    return A, B, C
 end
 
 # -------- Auxiliary Functions -------- #
@@ -620,9 +570,8 @@ function sort_gleb!(exp_vectors::Vector{PointVector{ZZRingElem}})
 end
 
 function sort_gleb_max!(exp_vectors::Vector{PointVector{ZZRingElem}})
-    sort!(exp_vectors, by = s -> (
-    s[1] == 0 ? (0, s[2:end]...) : (1, sum(s), s[end:-1:1]...)
-))
+    sort!(exp_vectors, by = s -> (s[1] == 0 ? (0, sum(s), s[end:-1:1]...) : (1, sum(s), s[end:-1:1]...)))
 end
+
 
 # ————————————————— #
